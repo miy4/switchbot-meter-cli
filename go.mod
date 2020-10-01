3b0b505145e19b38de94fb965c1dedcbd18ff288
miy4 <miy4t7l@gmail.com>
2020-10-02 05:00:49 +0900

    add skeleton files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/miy4/switchbot-meter-cli
+
+go 1.15
+
+require github.com/spf13/cobra v1.0.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/miy4/switchbot-meter-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
